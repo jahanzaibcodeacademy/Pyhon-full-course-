9945626a0c28cc15fa38fbf1c049c83e251fce1f
jahanzaibcodeacademy <jahanzaibcodeacademy@gmail.com>
2024-10-25 15:59:12 +0500

    Python full course with playlist and source code

---
+++ ch01.py
@@ -0,0 +1,46 @@
+print("This is the python")
+
+# Comments
+
+# This is a single line comment
+
+# This is a multiline comment
+'''
+This is a multiline comment
+In which multiple lines can be written'''
+
+"""
+This is a multiline comment
+In which multiple lines can be written"""
+
+# Variables
+
+x = 22 #integer
+y = 20 #integer
+print(x)
+print(y)
+
+a = 'Khan' # string
+print(a)
+
+aa = 20.5 # float
+print(aa)
+
+bb = True # boolean
+print(bb)
+
+cc = None # NoneType
+print(cc)
+
+dd = 20+3j # complex
+print(dd)
+
+# Data types
+
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(aa))
+print(type(bb))
+print(type(cc))
+print(type(dd))
